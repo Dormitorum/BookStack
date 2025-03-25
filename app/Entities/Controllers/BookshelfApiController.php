162494720ac1c6fd2d34614fa376d82eac0fe7ce
Omar Hernández <omar.hernandez@dormitorum.com>
2025-03-25 12:45:09 +0000

    Update BookshelfApiController.php

--- app/Entities/Controllers/BookshelfApiController.php
+++ app/Entities/Controllers/BookshelfApiController.php
@@ -26,7 +26,7 @@ class BookshelfApiController extends ApiController
     {
         $shelves = $this->queries
             ->visibleForList()
-            ->with(['cover:id,name,url', 'permissions'])
+            ->with(['cover:id,name,url', 'permissions.role:id,display_name'])
             ->addSelect(['created_by', 'updated_by']);
 
         return $this->apiListingResponse($shelves, [
@@ -66,7 +66,7 @@ class BookshelfApiController extends ApiController
             'books' => function (BelongsToMany $query) {
                 $query->scopes('visible')->get(['id', 'name', 'slug']);
             },
-            'permissions'
+            'permissions.role:id,display_name'
         ]);
 
         return response()->json($shelf);
